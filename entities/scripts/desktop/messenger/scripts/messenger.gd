435c23cd86fa18c8e7c7ad06b1ebabb1daa19cb3
jackoccl <jackoccleston@gmail.com>
2023-11-29 22:21:11 -0700

    Messenger tabs

--- entities/scripts/desktop/messenger/scripts/messenger.gd
+++ entities/scripts/desktop/messenger/scripts/messenger.gd
@@ -4,28 +4,30 @@ var messageScene = preload("res://entities/scripts/desktop/messenger/components/
 var responseScene = preload("res://entities/scripts/desktop/messenger/components/response_message.tscn")
 var msgVboxScene
 @onready var msg = messenger_msg.new("Hello")
-@onready var msgVbox = $Control/HBoxMain/VBoxRight/MessagePanel/VScrollBar/MessageTabContainer/VBoxMessages
-@onready var MsgTab = $Control/HBoxMain/VBoxRight/MessagePanel/VScrollBar/MessageTabContainer
+#@onready var msgVbox = $Control/HBoxMain/VBoxRight/MessagePanel/VScrollBar/MessageTabContainer/VBoxMessages
+@onready var msgTab = $Control/HBoxMain/VBoxRight/MessagePanel/VScrollBar/MessageTabContainer
 @onready var msgEdit = $Control/HBoxMain/VBoxRight/Panel2/MessageEdit
-@onready var msgArr = []
+@onready var currentTab = msgTab.current_tab
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	var newMsg = messageScene.instantiate()
-	newMsg.init(msg)
-	msgVbox.add_child(newMsg)
+#	var newMsg = messageScene.instantiate()
+#	newMsg.init(msg)
+#	msgVbox.add_child(newMsg)
 	
-	for i in $Control/HBoxMain/VBoxFriends/FriendButtons.get_child_count():
+	for i in $Control/HBoxMain/VBoxFriends/Node.get_child_count():
 		var vbox = VBoxContainer.new()
 		vbox.name = str(i)
-		MsgTab.add_child(vbox)
-		
+		msgTab.add_child(vbox)
+	
+	
+	print(msgTab.current_tab)
 	
 	
 		
 
-func createResponse():
+func createResponse(msgVbox):
 		var newMsg = messenger_msg.new(msgEdit.text)
 		var response = responseScene.instantiate()
 		response.init(newMsg)
@@ -37,5 +39,36 @@ func createResponse():
 func _process(delta: float) -> void:
 	if msgEdit.text != "" and msgEdit.has_focus():
 		if Input.is_action_just_pressed("Enter"):
-			createResponse()
+			createResponse(msgTab.get_child(currentTab))
+	
 
+
+
+
+
+
+
+
+
+func _on_button_pressed() -> void:
+	msgTab.current_tab = 0
+	currentTab = 0
+	
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	msgTab.current_tab = 1
+	currentTab = 1
+	
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	msgTab.current_tab = 2
+	currentTab = 2
+
+
+func _on_button_4_pressed() -> void:
+	msgTab.current_tab = 3
+	currentTab = 3
